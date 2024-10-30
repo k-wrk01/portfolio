b2d7ee2ed8ef597436ab756491bd3f2f012e3f5f
k-wrk01 <k.wrk01@gmail.com>
2024-10-31 01:00:22 +0900

    fix top

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,8 @@ import { Works } from './components/Works';
 import { Profile } from './components/Profile';
 import { Footer } from './components/Footer';
 
+import image from './top.png';
+
 function App() {
   const [isLoading, setIsLoading] = useState(true)
   useEffect(() => {
@@ -22,7 +24,7 @@ function App() {
         </div>
         : <div>
           <Header />
-          <div className="hero h-[calc(100vh-4rem)]">
+          <div className="hero h-[calc(100vh-4rem)]" style={{backgroundImage: `url(${image})`}}>
             <div className="hero-content text-center">
               <h1 className="mb-5 md:text-9xl text-6xl font-bold animate-tracking-in-expand">Portfolio</h1>
             </div>
